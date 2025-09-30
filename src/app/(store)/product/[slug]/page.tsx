e5f5a2cec8c474f7e6b8430040f84671058794ca
Luiz Eduardo <luizeduardo1953@gmail.com>
2025-09-29 22:10:58 -0300

    Add Page Product

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,10 +1,35 @@
 import Image from 'next/image';
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product';
 
-export default function ProductPage() {
+interface ProductProps {
+    params: {
+        slug: string
+    }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+    const response = await api(`/products/${slug}`, {
+        next: {
+            revalidate: 60 * 60,
+        },
+    })
+
+    const product = await response.json()
+
+    return product
+
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+
+    const product = await getProduct(params.slug)
+
     return (
         <div className="relative grid max-h[860px] grid-cols-3">
             <div className="col-span-2 overflow-hidden">
-                <Image src="/moletom-never-stop-learning.png"
+                <Image 
+                    src={product.image}
                     alt="product"
                     width={1000}
                     height={1000}
@@ -13,16 +38,27 @@ export default function ProductPage() {
 
             <div className="flex flex-col justify-center px-12">
                 <h1 className="text-3xl font-bold leading-tight">
-                    Moletom never stop Learning
+                    {product.title}
                 </h1>
 
                 <p className='mt-2 leading-relaxed text-zinc-400'>
-                    Moletom fabricado com algodão e 12% de poliéster.
+                    {product.description}
                 </p>
 
                 <div className='mt-8 flex items-center gap-3'>
-                    <span className='inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold'>R$129</span>
-                    <span className='text-sm text-zinc-400'>Em 12x s/ juros de R$10,75</span>
+                    <span className='inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold'>{product.price.toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                        maximumFractionDigits: 0,
+                        minimumFractionDigits: 0
+                    })}
+                    </span>
+                    
+                    <span className='text-sm text-zinc-400'>Em 12x s/ juros de R$ {(product.price / 12).toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    })}
+                    </span>
                 </div>
 
                 <div className='mt-8 space-y-4'>
