6af535897890087f54f0a57ba1831d4526498b89
Luiz Eduardo <luizeduardo1953@gmail.com>
2025-10-03 21:27:51 -0300

    Created Context for Cart

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,15 @@
 // Layout para seção da loja, tudo que estiver dentro de /store/* vai usar esse layout
 
+import { CartProvider } from "@/context/cart-context";
 import Header from "../../components/header";
 
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
   return (
-  <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 px-8 py-8">
-    <Header />
-    {children}
-  </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 px-8 py-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
