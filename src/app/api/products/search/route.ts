4cc9541fef9fa851f9faa265892bb510a34bfcfe
Luiz Eduardo <luizeduardo1953@gmail.com>
2025-10-06 10:16:38 -0300

    Add Route Search for products

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse, NextRequest } from 'next/server'
+import data from '../data.json'
+import { z } from 'zod'
+
+export async function GET( 
+    request: NextRequest
+){
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+    const { searchParams } = request.nextUrl
+
+    const query = z.string().parse(searchParams.get('q'))
+
+    const products = data.products.filter(product => {
+        return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    })
+
+    return Response.json(products)
+}
+
+// localhost:3000/api/product/search?q=moletom
